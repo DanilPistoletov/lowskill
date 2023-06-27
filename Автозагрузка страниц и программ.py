767a3404fa609e31fe7406b4a982b784c8604274
Данил Пистолетов <97846638+DanilPistoletov@users.noreply.github.com>
2023-06-27 12:34:27 +0300

    Add files via upload

---
+++ Автозагрузка страниц и программ.py
@@ -0,0 +1,5 @@
+import webbrowser
+import subprocess
+webbrowser.open('https://vk.com/im', new=2)
+webbrowser.open('https://e.mail.ru', new=2)
+subprocess.Popen('C:\Program Files\Soft\Telegram Desktop\Telegram.exe')
